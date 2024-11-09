08a14d2c87e61ed2a758ed6290fafa26118b2546
jason <jason@hotmail.com>
2024-11-09 13:31:00 +0800

    fx
    fx

--- pkg/utils/httpc/httpc.go
+++ pkg/utils/httpc/httpc.go
@@ -13,10 +13,15 @@ var once sync.Once
 func init() {
 	once.Do(func() {
 		//TODO 这儿看是否要实现链接池那些 https://github.com/go-resty/resty
-		engine = resty.New()
+		engine = MustNewHttpClient()
 	})
 }
 
 func Do(ctx context.Context) *resty.Request {
 	return engine.R().SetContext(ctx)
+}
+
+// MustNewHttpClient new http client
+func MustNewHttpClient() *resty.Client {
+	return resty.New()
 }
